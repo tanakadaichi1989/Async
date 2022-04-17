5b3e7be6578e7b61e5eac659d509e6a5d7165430
Daichi Tanaka <tanakadaichi_1989@icloud.com>
2022-04-18 01:06:51 +0900

    Initial Commit

---
+++ Async/AsyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncApp.swift
+//  Async
+//
+//  Created by 田中大地 on 2022/04/18.
+//
+
+import SwiftUI
+
+@main
+struct AsyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
