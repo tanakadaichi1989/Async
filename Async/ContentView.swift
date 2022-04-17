e542ab5ca31bd83c9ab09d50f7875f1552d66865
Daichi Tanaka <tanakadaichi_1989@icloud.com>
2022-04-18 01:56:38 +0900

    first commit

--- Async/ContentView.swift
+++ Async/ContentView.swift
@@ -6,11 +6,70 @@
 //
 
 import SwiftUI
+import Dispatch
 
 struct ContentView: View {
+    @State var progress = 0
+    
+    var title: String {
+        switch progress {
+        case 0: return ""
+        case 0..<100: return "Downloading ..."
+        case 100: return "Done"
+        default: return ""
+        }
+    }
+    
+    var canTap: Bool {
+        switch progress {
+        case 0: return true
+        case 1..<100: return false
+        case 100: return true
+        default: return true
+        }
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("\(progress) %")
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+            
+            ProgressView(title,value: Float(progress), total: 100)
+                .padding()
+            
+            Button {
+                method { result in
+                    progress = result
+                }
+            } label: {
+                Text("PUSH")
+                    .fontWeight(.heavy)
+            }
+            .buttonStyle(.borderedProminent)
+            .disabled(!canTap)
+        }
+    }
+    
+    private func method(completion: @escaping (Int) -> Void){
+        var result = 0
+        let global = DispatchQueue.global()
+        let main = DispatchQueue.main
+        
+        global.asyncAfter(deadline: .now() + 1) {
+            while(result < 100){
+                sleep(1)
+                main.async {
+                    completion(result)
+                }
+                if result > 90 {
+                    result += 1
+                } else {
+                    result += Int.random(in: Range(1...10))
+                }
+            }
+            
+        }
     }
 }
 
